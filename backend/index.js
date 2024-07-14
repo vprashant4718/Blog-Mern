8f96587c88fee48e11c0557988e084671f0469a1
vprashant4718 <127789716+vprashant4718@users.noreply.github.com>
2024-07-14 15:30:39 +0530

    connection to mongo

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_CONNECT).then(()=>{console.log('Connected to Mongo')});
 
 const app = express();
 
 app.listen(5000, (req,res)=>{
-    console.log('Server is running on port 5000')
+    console.log('Server is running on port 5000');
+    
 })
